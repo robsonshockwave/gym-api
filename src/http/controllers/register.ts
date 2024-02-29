0198034b1f305adfdfb68ca31e3775db8d122867
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-28 21:01:55 -0300

    separando a camada de caso de uso do register da camada http

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
-import { hash } from 'bcryptjs';
+import { registerUseCase } from '@/use-cases/register';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,25 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (user) {
+  try {
+    registerUseCase({ name, email, password });
+  } catch (error) {
     return reply.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
