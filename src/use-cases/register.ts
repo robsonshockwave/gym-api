d50f7f9f6a00312a4d3fe582b1d2191d442115ed
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-28 21:58:30 -0300

    lidando com erros do use case no controller

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (user) {
-      throw new Error('Email already exists');
+      throw new UserAlreadyExistsError();
     }
 
     await this.usersRepository.create({
