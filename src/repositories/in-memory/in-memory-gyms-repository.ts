9be3ae231fd01bf38a43c1d388cd699026b6e47d
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-03-03 14:52:42 -0300

    caso de uso/teste de busca de academia com paginação

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -29,4 +29,10 @@ export class InMemoryGymsRepository implements GymsRepository {
 
     return gym;
   }
+
+  async searchMany(query: string, page: number) {
+    return this.gyms
+      .filter((gym) => gym.title.toLowerCase().includes(query.toLowerCase()))
+      .slice((page - 1) * 20, page * 20);
+  }
 }
