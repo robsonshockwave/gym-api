50951972a073b0dc12183e1bd79cf87becf45720
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-03-03 19:22:57 -0300

    validação e teste do horario do checkin

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -4,6 +4,8 @@ import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error';
 import { MaxDistanceError } from './errors/max-distance-error';
+import dayjs from 'dayjs';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
@@ -23,6 +25,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validated_at = new Date();
