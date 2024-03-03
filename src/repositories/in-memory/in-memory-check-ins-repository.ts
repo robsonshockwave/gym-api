dcd27b34912050f892b5b491b8432a5658d08f4b
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-03-03 14:36:13 -0300

    caso de uso/teste listagem do historico

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -39,4 +39,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return newCheckIn;
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.checkIns
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
 }
