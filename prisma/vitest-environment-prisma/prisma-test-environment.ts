c4281c20e8868c34fb87d433c2f9bab9de367580
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-03-09 22:52:01 -0300

    test environment do prisma config

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
+
+const prima = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   transformMode: 'web',
   name: 'prisma',
   async setup() {
-    console.log('Setup');
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy');
+
+    console.log(generateDatabaseURL(schema));
 
     return {
       async teardown() {
-        console.log('Teardown');
+        await prima.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prima.$disconnect();
       },
     };
   },
