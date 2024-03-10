f4a9047fc09700e2eb05db295cb6d169b524470f
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-03-09 23:01:02 -0300

    teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest';
+import { app } from '@/app';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {});
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Robson Arruda',
+      email: 'robson@teste.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
